1624f081211cc21b40ae9f62257c01318c5364a6
Kevin Ruiz Santana <124311914+Kevinrsantana2@users.noreply.github.com>
2023-03-23 13:06:02 -0700

    if birthdayDictionary.get(nameFind):

--- birthdayExample (1).py
+++ birthdayExample (1).py
@@ -1,23 +1,9 @@
 # Kevin Ruiz Santana 2023
 import json
-
-#
 # this is a relative path to the .json data file
-# you can also use a "full" or "absolute path" to the file
-# windows and mac paths are different.  You should google and youttube to learn about paths if you are
-# not familiar with them.  They are important fundamental computer concepts.
-#
-# this is a full windows path, note the forward slashes "/" used in python
-# pathToFile = "E:/Users/jerome/GitHub/evc-cit134a-python/birthday/birthday.json"
-#
-# mac (which is built on linux) and linux paths are like this: "a/b/c/d/e/f.json"
-#
-
-# relative path
-# pathToFile = "./birthday/birthday.json"
+# pathToFile = "birthday (1).json"
 #relative path to file
 pathToFile = "birthday (1).json"
-
 
 # try to open a file and throw a error if it is not found
 try:
